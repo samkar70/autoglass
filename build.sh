cb7a4c7a15ad923191d4bf70bd3f031c2d486841
samkar70 <samkar70@gmail.com>
2025-09-25 13:06:12 -0500

    Forzar actualización de permisos de build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
+# Build script for BMS Auto Glass on Render
+
 set -o errexit
 
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
