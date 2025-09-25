af6c425ccf6c1c55bc05abcb323a6e2b7f9b5592
samkar70 <samkar70@gmail.com>
2025-09-25 12:19:22 -0500

    Añadir redirección de la página principal

--- pos_project/urls.py
+++ pos_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView # <-- AÑADE ESTA LÍNEA
 
 urlpatterns = [
+    # AÑADE ESTA NUEVA RUTA:
+    # Redirige la página principal (/) a /ordenes/
+    path('', RedirectView.as_view(url='/ordenes/', permanent=True)),
+
     path('admin/', admin.site.urls),
     path('ordenes/', include('ordenes.urls')),
 ]
